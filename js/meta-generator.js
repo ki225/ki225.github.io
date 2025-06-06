10b39d1c837f24c63ac6590be0e65585036802df
ki225 <271yeye@gmail.com>
2025-06-06 10:17:43 +0800

    Site updated: 2025-06-06 10:17:37

---
+++ js/meta-generator.js
@@ -0,0 +1,22 @@
+// Usage: hexo generate
+hexo.extend.generator.register('meta', function(locals) {
+  var meta = {
+    tags: [],
+    posts: [],
+    categories: []
+  }
+  locals.tags.sort('name').each(function(tag) {
+    meta.tags.push(tag.name)
+  })
+  locals.categories.sort('name').each(function(category) {
+    meta.categories.push(category.name)
+  })
+  locals.posts.sort('name').each(function(post) {
+    meta.posts.push({
+      title: post.title,
+      url: encodeURI(post.permalink),
+      date: post.updated.toDate().toISOString() || post.date.toDate().toISOString()
+    })
+  })
+  return {path: 'meta.json', data: JSON.stringify(meta)}
+})
