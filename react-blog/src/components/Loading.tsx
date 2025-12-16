a2c0fea506d69f8ce190997600cbcc5bf4d3ec1e
ki225 <271yeye@gmail.com>
2025-12-17 01:52:56 +0800

    feat: loading animation

---
+++ react-blog/src/components/Loading.tsx
@@ -0,0 +1,41 @@
+import "../styles/Loading.css";
+import "../styles/LoadingAlt.css";
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="pixel-cat-wrapper">
+        <div className="pixel-cat cat-frame-1">
+          <div className="pixel-row row-1"></div>
+          <div className="pixel-row row-2"></div>
+          <div className="pixel-row row-3"></div>
+          <div className="pixel-row row-4"></div>
+          <div className="pixel-row row-5"></div>
+          <div className="pixel-row row-6"></div>
+          <div className="pixel-row row-7"></div>
+          <div className="pixel-row row-8"></div>
+          <div className="pixel-row row-9"></div>
+          <div className="pixel-row row-10"></div>
+          <div className="pixel-row row-11"></div>
+          <div className="pixel-row row-12"></div>
+        </div>
+        <div className="pixel-cat cat-frame-2">
+          <div className="pixel-row row-1"></div>
+          <div className="pixel-row row-2"></div>
+          <div className="pixel-row row-3"></div>
+          <div className="pixel-row row-4"></div>
+          <div className="pixel-row row-5"></div>
+          <div className="pixel-row row-6"></div>
+          <div className="pixel-row row-7"></div>
+          <div className="pixel-row row-8"></div>
+          <div className="pixel-row row-9"></div>
+          <div className="pixel-row row-10-alt"></div>
+          <div className="pixel-row row-11-alt"></div>
+          <div className="pixel-row row-12"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
